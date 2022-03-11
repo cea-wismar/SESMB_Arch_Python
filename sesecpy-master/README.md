74e680442a3171d8f953f62303679eacae444eef
CEA Wismar <71314407+cea-wismar@users.noreply.github.com>
2022-03-11 11:38:04 +0100

    Add files via upload

---
+++ sesecpy-master/README.md
@@ -0,0 +1,71 @@
+INTRODUCTION
+
+The software SESEcPy has been developed by the research group Computational
+Engineering and Automation (CEA) at Wismar University of Applied Sciences.
+The software implements an Experiment Control according to the System Entity
+Structure / Model Base (SES/MB) infrastructure introduced for automatic
+execution of simulation experiments.
+Please read the documentation for further information. A comprehensive
+introduction to the SES/MB theory is given in the documentation of the SES
+modeling tool SESToPy.
+The software is written in Python3.
+It was tested with the simulation softwares Matlab R2018a (for Simulink),
+OpenModelica 1.12.0, and Dymola 2018.
+
+EXECUTE
+
+Copy SESToPy, SESMoPy, SESEuPy, and SESEcPy in one directory "SESMB_Infrastructure"
+like shown below.
+
+SESMB_Infrastructure  
+    |  
+     -- SESEcPy  
+    |  
+     -- SESEuPy  
+    |  
+     -- SESMoPy  
+    |  
+     -- SESToPy
+
+Copy the directory SESMB_Infrastructure in your home folder, e.g. C:\Users\\\<Username> 
+(necessary for usage of Dymola as simulator, otherwise Dymola cannot simulate).
+Make sure, that the commands "omc", "matlab", and "dymola" are on the path and the
+programs are thus startable from the shell (depending on which simulator you want
+to use). This is described in the documentation of SESEuPy in detail.  
+The program can be executed from source. Python3 needs to be installed and the
+Python executable needs to be on the path. Open a shell and change with the cd
+command to the SESEcPy directory. The program then can be started with the shell
+command:
+- in Windows: python main.py
+- in Linux: python3 main.py
+
+CHANGELOG
+
+
+KNOWN BUGS, NOTES, TODO
+
+
+LICENSE
+
+This application is licensed under GNU GPLv3.
+
+HOW TO CITE
+
+Folkerts, H., Pawletta, T., Deatcu, C., and Hartmann, S. (2019). A Python Framework for
+Model Specification and Automatic Model Generation for Multiple Simulators. In: Proc. of
+ASIM Workshop 2019 - ARGESIM Report 57, ASIM Mitteilung AM 170. ARGESIM/ASIM Pub.
+TU Vienna, Austria, 02/2019, 69-75. (Print ISBN 978-3-901608-06-3)
+
+Folkerts, H., Deatcu, C., Pawletta, T., Hartmann, S. (2019). Python-Based eSES/MB
+Framework: Model Specification and Automatic Model Generation for Multiple Simulators.
+SNE - Simulation Notes Europe Journal, ARGESIM Pub. Vienna, SNE 29(4)2019, 207-215.
+(DOI: 10.11128/sne.29.tn.10497),(Selected EUROSIM 2019 Postconf. Publ.)
+
+Folkerts, H., Pawletta, T., Deatcu, C., Santucci, J.F., Capocchi, L. (2019). An Integrated
+Modeling, Simulation and Experimentation Environment in Python Based on SES/MB and DEVS.
+Proc. of the 2019 Summer Simulation Conference, ACM Digital Lib., 2019 July 22-24, Berlin,
+Germany, 12 pages.
+
+Folkerts, H., Pawletta, T., Deatcu, C. (2021). Model Generation for Multiple Simulators
+Using SES/MB and FMI. SNE - Simulation Notes Europe Journal, ARGESIM Pub. Vienna,
+SNE 31(1) 2021, 25-32. (DOI: 10.11128/sne.31.tn.10554), (Selected ASIM 2020 Postconf. Publ.)
---
+++ sesecpy-master/main.py
@@ -0,0 +1,108 @@
+#Experiment Control
+#The Experiment Control consists of an experiment independent and an experiment dependent part.
+#This is the startscript managing the call of both.
+
+import os
+import platform
+import shutil
+from sesecpyExperiment import *
+from sesecpyGeneral import *
+
+
+#define the python call (python / python3) and variables depending on the OS
+system = platform.system()
+if system == "Windows":
+    pythoncall = "python"
+    dirSep = "\\"
+else:
+    pythoncall = "python3"
+    dirSep = "/"
+
+#set directories
+#get the current directory of this script and split it
+sesecpyPathParts = os.path.dirname(os.path.realpath(__file__)).split(dirSep)
+#set the directory for the infrastructure -> it is one directory up of the directory of this script
+infrastructurePath = dirSep.join(sesecpyPathParts[:-1])
+
+#create objects
+sesecpyExperimentObject = sesecpyExperiment
+sesecpyGeneralObject = sesecpyGeneral
+
+#initial settings -> experiment specific
+sesFilePath, mbFilespath, deletePES, deleteFPES, deleteModelsAfterSimulation = sesecpyExperimentObject.initialSettings(sesecpyExperiment)
+
+#begin the loop -> as long as the goals are not met -> execute in loop
+loopCounter = 0
+loopMax = 10
+goalsMet = False
+interface = ""
+addedMbFiles = []
+resultfile = ""
+errorOcc = False
+overallResults = ""
+while loopCounter < loopMax and not goalsMet and not errorOcc:
+
+    #calculate the next SESvars -> experiment specific
+    goalsMet, sesvars, pesFilePath, fpesFilePath, simulator, interface, appendConfig, overallResults = sesecpyExperimentObject.nextState(sesecpyExperiment, resultfile, loopCounter)  # the loopCounter is the simulation number
+
+    if system != "Windows":
+        pesFilePath = pesFilePath.replace("\\", "/")
+        fpesFilePath = fpesFilePath.replace("\\", "/")
+
+    #select the sesfile depending on the interface -> only do in the first loop, then it is set
+    if loopCounter == 0 and interface == "native":
+        sesFilePath = sesFilePath.get("native")
+    elif loopCounter == 0 and interface == "FMI":
+        sesFilePath = sesFilePath.get("FMI")
+
+    if system != "Windows":
+        sesFilePath = sesFilePath.replace("\\", "/")
+
+    #execute the prune, flatten, build and execute toolchain if the goals are not met yet -> general
+    if not goalsMet:
+
+        if interface == "native":   #for the native interface -> copy the MB depending on the simulator
+            usedMB = mbFilespath.get(simulator)
+        else:                       #for FMI -> copy the only MB (for all simulators)
+            usedMB = mbFilespath.get(interface)
+
+        if system != "Windows":
+            usedMB = usedMB.replace("\\", "/")
+
+        for hfile in os.listdir(usedMB):
+            hfilepathname = os.path.join(usedMB, hfile)
+            newhfilepathname = os.path.join(os.path.split(sesFilePath)[0], hfile)
+            if not os.path.exists(newhfilepathname):
+                try:
+                    if os.path.isdir(hfilepathname):
+                        shutil.copytree(hfilepathname, newhfilepathname)
+                    else:
+                        shutil.copyfile(hfilepathname, newhfilepathname)
+                except:
+                    print("Error copying the MB file(s) in the directory with the SES file!")
+                addedMbFiles.append(newhfilepathname)
+
+        resultfile = sesecpyGeneralObject.executeToolchain(sesecpyGeneral, infrastructurePath, pythoncall, sesFilePath, sesvars, pesFilePath, fpesFilePath, deletePES, deleteFPES, deleteModelsAfterSimulation, appendConfig)
+        if resultfile == "":
+            errorOcc = True
+
+        #remove the copied MB from the directory containing the FPES
+        # -> for the native interface (in the next loop maybe there are other MB files for a different simulator necessary, which can have the same name)
+        # -> for FMI it is always the same MB -> do not remove
+        if interface == "native":
+            for addedMbFile in addedMbFiles:
+                if os.path.exists(addedMbFile):
+                    if os.path.isdir(addedMbFile):
+                        shutil.rmtree(addedMbFile)
+                    else:
+                        os.remove(addedMbFile)
+
+    loopCounter += 1
+
+#calculate overall results -> experiment specific
+if not goalsMet and loopCounter == loopMax and not errorOcc:
+    print("Finished. The goal could not be met in " + str(loopMax) + " steps or another error occurred. Please look at the output in the command window.")
+elif goalsMet and not errorOcc:
+    print("Finished. The goal is met with:\n" + overallResults)
+elif errorOcc:
+    print("An error occurred.")
